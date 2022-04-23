0442d3d249b6c081beb603bd1f234238bdb35471
IlMike22 <michaelwidlok@gmail.com>
2022-04-23 16:56:59 +0200

    add the tracked food item ui

--- tracker/tracker_presentation/src/main/java/com/mind/market/tracker_presentation/components/NutrientInfo.kt
+++ tracker/tracker_presentation/src/main/java/com/mind/market/tracker_presentation/components/NutrientInfo.kt
@@ -1,48 +1,47 @@
 package com.mind.market.tracker_presentation.components
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.LastBaseline
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 import com.mind.market.core_ui.LocalSpacing
 
 @Composable
-fun UnitDisplay(
+fun NutrientInfo(
+    name: String,
     amount: Int,
     unit: String,
     modifier: Modifier = Modifier,
     amountTextSize: TextUnit = 20.sp,
     amountColor: Color = MaterialTheme.colors.onBackground,
     unitTextSize: TextUnit = 14.sp,
-    unitColor: Color = MaterialTheme.colors.onBackground
+    unitColor: Color = MaterialTheme.colors.onBackground,
+    nameTextStyle: TextStyle = MaterialTheme.typography.body1
 ) {
-    val spacing = LocalSpacing.current
-    Row(
-        modifier = modifier
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = amount.toString(),
-            style = MaterialTheme.typography.h1,
-            fontSize = amountTextSize,
-            color = amountColor,
-            modifier = Modifier.alignBy(LastBaseline)
+        UnitDisplay(
+            amount = amount,
+            unit = unit,
+            amountTextSize = amountTextSize,
+            amountColor = amountColor,
+            unitTextSize = unitTextSize,
+            unitColor = unitColor
         )
-
-        Spacer(modifier = Modifier.width(spacing.spaceExtraSmall))
-
         Text(
-            text = unit,
-            style = MaterialTheme.typography.body1,
-            fontSize = unitTextSize,
-            color = unitColor,
-            modifier = Modifier.alignBy(LastBaseline)
+            text = name,
+            color = MaterialTheme.colors.onBackground,
+            style = nameTextStyle,
+            fontWeight = FontWeight.Light
         )
     }
 }
--- tracker/tracker_presentation/src/main/java/com/mind/market/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/mind/market/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -2,14 +2,16 @@ package com.mind.market.tracker_presentation.tracker_overview
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.mind.market.core.util.UiEvent
 import com.mind.market.core_ui.LocalSpacing
-import com.mind.market.tracker_presentation.tracker_overview.components.NutrientsHeader
-import com.mind.market.tracker_presentation.tracker_overview.components.TrackerDaySelector
+import com.mind.market.tracker_presentation.R
+import com.mind.market.tracker_presentation.tracker_overview.components.*
 
 @Composable
 fun TrackerOverviewScreen(
@@ -40,6 +42,50 @@ fun TrackerOverviewScreen(
             )
 
             Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        }
+
+        items(state.meals) { meal ->
+            ExpendableMeal(
+                meal = meal,
+                onToggleClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnToggleMealClick(meal))
+                },
+                content = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = spacing.spaceSmall)
+                    ) {
+                        state.trackedFoods.forEach { food ->
+                            TrackedFoodItem(
+                                onDeleteClick = {
+                                    viewModel.onEvent(
+                                        TrackerOverviewEvent.OnDeleteTrackedFoodClick(
+                                            food
+                                        )
+                                    )
+                                },
+                                trackedFood = food
+                            )
+
+                            Spacer(Modifier.height(spacing.spaceMedium))
+                        }
+
+                        AddButton(
+                            text = stringResource(
+                                id = R.string.add_meal,
+                                meal.name.asString(context)
+                            ),
+                            onClick = {
+                                viewModel.onEvent(TrackerOverviewEvent.OnAddFoodClick(meal))
+                            },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                },
+
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
